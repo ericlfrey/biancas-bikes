08692b326fa9b00cd69fa175104f06e0ae64b649
Eric Frey <ericlfrey@gmail.com>
2024-02-09 12:49:44 -0600

    Added logic to assign employee to WO.

--- client/src/components/workorders/WorkOrderList.jsx
+++ client/src/components/workorders/WorkOrderList.jsx
@@ -1,14 +1,36 @@
 import { useEffect, useState } from 'react';
-import { Table } from 'reactstrap';
-import { getIncompleteWorkOrders } from '../../managers/workOrderManager';
+import { Input, Table } from 'reactstrap';
+import {
+  getIncompleteWorkOrders,
+  updateWorkOrder,
+} from '../../managers/workOrderManager';
 import { Link } from 'react-router-dom';
+import { getUserProfiles } from '../../managers/userProfileManager';
 
 export default function WorkOrderList({ loggedInUser }) {
   const [workOrders, setWorkOrders] = useState([]);
+  const [mechanics, setMechanics] = useState([]);
+
+  useEffect(() => {
+    getIncompleteWorkOrders().then(setWorkOrders);
+    getUserProfiles().then(setMechanics);
+  }, []);
 
   useEffect(() => {
     getIncompleteWorkOrders().then(setWorkOrders);
   }, []);
+
+  const assignMechanic = (workOrder, mechanicId) => {
+    const clone = structuredClone(workOrder);
+    clone.userProfileId = mechanicId || null;
+    updateWorkOrder(clone).then(() => {
+      getIncompleteWorkOrders().then(setWorkOrders);
+    });
+  };
+
+  const completeWorkOrder = workOrderId => {
+    console.log(`Completed ${workOrderId}`);
+  };
 
   return (
     <>
@@ -35,11 +57,38 @@ export default function WorkOrderList({ loggedInUser }) {
               <td>{wo.description}</td>
               <td>{new Date(wo.dateInitiated).toLocaleDateString()}</td>
               <td>
+                <Input
+                  type="select"
+                  onChange={e => {
+                    assignMechanic(wo, parseInt(e.target.value));
+                  }}
+                  value={wo.userProfileId || 0}
+                >
+                  <option value="0">Choose mechanic</option>
+                  {mechanics.map(m => (
+                    <option
+                      key={m.id}
+                      value={m.id}
+                    >{`${m.firstName} ${m.lastName}`}</option>
+                  ))}
+                </Input>
+              </td>
+              <td>
+                {wo.userProfile && (
+                  <Button
+                    onClick={() => completeWorkOrder(wo.id)}
+                    color="success"
+                  >
+                    Mark as Complete
+                  </Button>
+                )}
+              </td>
+              {/* <td>
                 {wo.userProfile
                   ? `${wo.userProfile.firstName} ${wo.userProfile.lastName}`
                   : 'unassigned'}
               </td>
-              <td></td>
+              <td></td> */}
             </tr>
           ))}
         </tbody>
